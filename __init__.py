70e25294da653c670ea3082bca5e614304f06982
Sid Mohan <sidmohan001@gmail.com>
2023-08-17 19:34:31 -0700

    cleanup

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# importing the modules in this directory
+
+from . import data_load_prep as dlp
+from . import train as tr
+from . import predict as pr
+from . import viz as vz
+from . import nan as nn
+from . import __init__ as init
+
+loading the modules one by one:
+
+
--- train.py
+++ train.py
@@ -25,57 +25,68 @@ cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cursor.fetchall()
 
 # Iterate over tables
+# Iterate over tables
 for table in tables:
     table_name = table[0]
 
-    # Read data into pandas DataFrame
-    query = f"SELECT * FROM \"{table_name}\""
-    df = pd.read_sql(query, conn)
-    
-    values = df['returns'].values # Get values from returns column
+    # Query to get the most recent close price from the 'c' column
+    query_recent_close = f"SELECT \"c\" FROM \"{table_name}\" ORDER BY ROWID DESC LIMIT 1"
+    most_recent_close_price = cursor.execute(query_recent_close).fetchone()[0]
 
-    training_data_len = math.ceil(len(values)* 0.8)
-    # print(training_data_len)
+    # Check if the most recent close price is above $20
+    if most_recent_close_price > 20:
+        # Query to select all data from the table
+        query = f"SELECT * FROM \"{table_name}\""
+        df = pd.read_sql(query, conn)
+
+        # ML analysis code goes here
+        # ...
+        print(f"Analyzing table {table_name} with most recent close price of ${most_recent_close_price:.2f}.")
+    
+        values = df['returns'].values # Get values from returns column
+
+        training_data_len = math.ceil(len(values)* 0.8)
+        # print(training_data_len)
     # Calculate daily returns and drop NaN values
 
     # print(df.head())
 
 
-    scaler = MinMaxScaler(feature_range=(0,1))
-    scaled_data = scaler.fit_transform(values.reshape(-1,1))
-    train_data = scaled_data[0: training_data_len, :]
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaled_data = scaler.fit_transform(values.reshape(-1,1))
+        train_data = scaled_data[0: training_data_len, :]
 
-    x_train = []
-    y_train = []
+        x_train = []
+        y_train = []
 
-    for i in range(60, len(train_data)):
-        x_train.append(train_data[i-60:i, 0])
-        y_train.append(train_data[i, 0])
-        
-    x_train, y_train = np.array(x_train), np.array(y_train)
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-
-    # Prepare Testing Data
-    test_data = scaled_data[training_data_len-60: , : ]
-    x_test = []
-    y_test = values[training_data_len:]
-
-    for i in range(60, len(test_data)):
-    x_test.append(values[i-60:i, 0])
-
-    x_test = np.array(x_test) 
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1)) # Reshape the data
+        for i in range(60, len(train_data)):
+            x_train.append(train_data[i-60:i, 0])
+            y_train.append(train_data[i, 0])
+            
+        x_train, y_train = np.array(x_train), np.array(y_train)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 
+        # Prepare Testing Data
+        test_data = scaled_data[training_data_len-60: , : ]
+        x_test = []
+        y_test = values[training_data_len:]
 
-    model = keras.Sequential()
-    model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(layers.LSTM(100, return_sequences=False))
-    model.add(layers.Dense(25))
-    model.add(layers.Dense(1))
-    model.summary()
+        for i in range(60, len(test_data)):
+            x_test.append(values[i-60:i, 0])
 
-    # Set Optimizer and Loss Function
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, batch_size= 1, epochs=4)
+            x_test = np.array(x_test) 
+            x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1)) # Reshape the data
+
+
+
+        model = keras.Sequential()
+        model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+        model.add(layers.LSTM(100, return_sequences=False))
+        model.add(layers.Dense(25))
+        model.add(layers.Dense(1))
+        model.summary()
+
+        # Set Optimizer and Loss Function
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        model.fit(x_train, y_train, batch_size= 1, epochs=4)
